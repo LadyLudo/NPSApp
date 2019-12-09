5831fe42130b473a1490a1e0567d47050ae87da9
Catherine Davis <catherinevtdavis@gmail.com>
2019-12-09 14:50:06 -0700

    finished js and changed a bit of html

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-'use strict'
+
 
 const APIKey = 'WJK9GqpmrJyNuGyLLzMgrmPp7FrWpGjt1RUKfFaV'
 const searchURL = 'https://developer.nps.gov/api/v1/parks'
@@ -8,12 +8,18 @@ function formatQueryParams(params) {
         .map(key => `${key}=${params[key]}`)
     return queryItems.join('&');
 }
+    const geoAPIKey = '7de1b0ccc7cc4ec4bd998b0e6370f8d5'
+    const geoSearchURL = 'https://api.opencagedata.com/geocode/v1/json'
 
 function displayResults(responseJson) {
     console.log(responseJson);
+    $('.error-message').empty();
     $('.results').empty();
+    if (responseJson.data.length === 0){
+        $('.error-message').text('There is no information for that state code, please try another.')
+    }
     for (let i=0; i < responseJson.data.length; i++) {
-        $('.results').append(
+            $('.results').append(
             `<li><h3>${responseJson.data[i].fullName}</h3>
             <p>${responseJson.data[i].description}</p>
             <p><a href="${responseJson.data[i].url}" target="_blank">${responseJson.data[i].url}</a></p>
@@ -44,6 +50,8 @@ function getStateParks(state, maxResults) {
         .catch(err => {
             $('.error-message').text(`Something went wrong: ${err.message}`);
         });
+
+
 }
 
 
