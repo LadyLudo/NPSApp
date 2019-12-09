c14a0863388d2e590ffeb8a8c2985a5bb99b44a3
Catherine Davis <catherinevtdavis@gmail.com>
2019-12-09 10:36:21 -0700

    added js functionality

---
+++ main.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const APIKey = 'WJK9GqpmrJyNuGyLLzMgrmPp7FrWpGjt1RUKfFaV'
+const searchURL = 'https://developer.nps.gov/api/v1/parks'
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+        .map(key => `${key}=${params[key]}`)
+    return queryItems.join('&');
+}
+
+function displayResults(responseJson) {
+    console.log(responseJson);
+    $('.results').empty();
+    for (let i=0; i < responseJson.data.length; i++) {
+        $('.results').append(
+            `<li><h3>${responseJson.data[i].fullName}</h3>
+            <p>${responseJson.data[i].description}</p>
+            <p><a href="${responseJson.data[i].url}" target="_blank">${responseJson.data[i].url}</a></p>
+            </li>`
+        )};
+    $('.results').removeClass('hidden');
+};
+
+function getStateParks(state, maxResults) {
+    const params= {
+        stateCode: state,
+        api_key: APIKey,
+        limit: maxResults,
+    };
+    const queryString = formatQueryParams(params)
+    const url= searchURL + '?' + queryString;
+
+    console.log(url);
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => {
+            $('.error-message').text(`Something went wrong: ${err.message}`);
+        });
+}
+
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const state= $('#state').val();
+        const maxResults= $('#maxResults').val();
+        getStateParks(state, maxResults);
+    });
+}
+$(watchForm());
